9d8cc23847dd9861dfe22c25f1cd10a86fa3aee0
Your NameNastya-20 <asyataa2016@gmail.com>
2024-09-13 22:20:24 +0300

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,34 +1,16 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import { PersistGate } from 'redux-persist/integration/react'; 
-import { store, persistor } from '../../redux/store';
-import { addContact, selectContacts } from '../../redux/contactsSlice';
+import { persistor } from '../../redux/store';
 import ContactForm from '../ContactForm/ContactForm';
 import SearchBox from '../SearchBox/SearchBox';
 import ContactList from '../ContactList/ContactList';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook } from '@fortawesome/free-solid-svg-icons';
-import initialContacts from '../../contacts.json';
 import Loader from '../Loader/Loader'; 
 import css from './App.module.css';
 
 const App = () => {
-  const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
-
-  useEffect(() => {
-    const savedContacts = localStorage.getItem('contacts');
-    if (!savedContacts) {
-      localStorage.setItem('contacts', JSON.stringify(initialContacts));
-      initialContacts.forEach(contact => dispatch(addContact(contact)));
-    }
-  }, [dispatch]);
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  return (
+   return (
     <PersistGate loading={<Loader />} persistor={persistor}>
       <h1 className={css.title}>
         <FontAwesomeIcon icon={faBook} className={css.iconBook} />
